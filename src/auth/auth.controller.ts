45501b19629faf199b40cd42a74262bb24523e64
Davyd Kondratenko <dajan526@gmail.com>
2025-03-18 20:19:17 +0100

    feat: initial. Auth, users modules, login endpoint
    Created project, implemented user and auth modules. Added login and
    profile and '' endpoints. Made profile endpoint protected and '' public.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { Public } from './public.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get()
+  findAll() {
+    return [];
+  }
+}
