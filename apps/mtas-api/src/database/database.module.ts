95dc63517fa4dc08e7d4abc738d6553de1ca1e92
Davyd Kondratenko <dajan526@gmail.com>
2026-05-03 18:50:28 +0200

    fix: path to migrations

--- apps/mtas-api/src/database/database.module.ts
+++ apps/mtas-api/src/database/database.module.ts
@@ -15,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
         entities: [__dirname + '/../**/*.entity.{js,ts}'],
-        migrations: [__dirname + '/../migrations/*.{js,ts}'],
+        migrations: [__dirname + '/migrations/*.{js,ts}'],
         migrationsRun: true,
         synchronize: false,
         ssl: configService.get('POSTGRES_SSL') === 'true',
